8039102f509170f0ecf5b0d9defc71d0acdea66d
David M. Lee <leedm777@yahoo.com>
2017-07-05 08:45:45 -0500

    Made TimeoutError portable
    I guess people want to use this lib in their browsers.

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // Copyright (c) 2015-2017 David M. Lee, II
 'use strict';
 
-/**
- * Exception indicating that the timeout expired.
- */
-var TimeoutError = module.exports.TimeoutError = function() {
-  Error.call(this)
-  Error.captureStackTrace(this, this.constructor);
-  this.message = 'Timeout';
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-require('util').inherits(TimeoutError, Error);
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/**
+ * Exception indicating that the timeout expired. Yes, the code looks odd. This
+ * is copy/pasta from the Babel.js repl to make the class extension portable.
+ */
+var TimeoutError = function (_Error) {
+  _inherits(TimeoutError, _Error);
+
+  function TimeoutError() {
+    _classCallCheck(this, TimeoutError);
+
+    return _possibleConstructorReturn(this, (TimeoutError.__proto__ || Object.getPrototypeOf(TimeoutError)).call(this, 'timeout'));
+  }
+
+  return TimeoutError;
+}(Error);
 
 /**
  * Rejects a promise with a {@link TimeoutError} if it does not settle within
